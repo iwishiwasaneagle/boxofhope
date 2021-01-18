3da234cb38b4af52e1b3caa42eda403122f436e0
iwishiwasaneagle <jh.ewers@gmail.com>
2021-01-18 13:53:38 +0000

    Initial commit

---
+++ cmask-gh-pages/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt='Logo'/>
+        <p>
+          Hi, welcome to CMask
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/iwishiwasaneagle/CMask"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Check this shit out!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
