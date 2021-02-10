ecc71c6f6c811263f88c7cf71c96059cf380fcad
iwishiwasaneagle <jh.ewers@gmail.com>
2021-02-10 12:37:59 +0000

    Documentation

--- servers/wiringPi-mock/wiringPi-mock.cpp
+++ servers/wiringPi-mock/wiringPi-mock.cpp
@@ -1,9 +1,6 @@
 #include "wiringPi.h"
 #include "version.h"
 
-/** Copy wiringPi version function
- * \return Major and minor version code via pointers
- **/
 void wiringPiVersion (int *major, int *minor)
 {
     *major = VERSION_MAJOR ;
