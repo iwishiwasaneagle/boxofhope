403c86d61272856260f14ca3f7ee9a11122ba690
iwishiwasaneagle <jh.ewers@gmail.com>
2021-02-10 12:57:35 +0000

    Door state system + more io server skeleton

--- servers/io_server/io_server.cpp
+++ servers/io_server/io_server.cpp
@@ -2,5 +2,38 @@
 
 int io::server_run(int argc, char* argv[]){
     std::cerr << "Starting IO Server" << std::endl;
+    
+
+    if(io::setup_io()>0){
+        std::cerr << "io::setup_io() exited with a non-zero exit code."<< std::endl;
+    };
+
+    while(1){
+        std::cerr << "io_server heartbeat" << std::endl;
+        delay(3000);
+    }
+
     return 0;
+}
+
+
+int io::setup_io(void){
+    
+    /// WiringPi setup function
+    wiringPiSetup();
+
+
+    /// Set door switch as input
+    pinMode(DOOR_SWITCH_PIN_WP,INPUT);
+
+    /// Setup door switch ISR on both rising and falling edge
+    wiringPiISR(DOOR_SWITCH_PIN_WP, INT_EDGE_BOTH, &io::door_switch_interrupt);
+
+    return 0;
+}
+
+void io::door_switch_interrupt(void){
+    io::isDoorOpen = digitalRead(DOOR_SWITCH_PIN_WP);
+    std::cout << "Is door open? " << io::isDoorOpen << std::endl;
+    delay(100); //< Delay for 100 to prevent bouncing
 }
--- servers/io_server/io_server.h
+++ servers/io_server/io_server.h
@@ -1,11 +1,16 @@
 #ifndef IO_SERVER
 #define IO_SERVER
 
+#define DOOR_SWITCH_PIN_WP 14
+#define UVC_LED_PIN_WP 15
+
 #include <iostream>
+#include "wiringPi.h"
 
 /// IO server namespace. Ensures that if the restful_server.h is imported, the same-named functions/class/etc. don't interact.
 namespace io{
-    /*! Handles the full life-cycle of the IO server.
+    /** 
+     * Handles the full life-cycle of the IO server.
      *
      * \param argc Argument count
      * \param argv Arguments as a list of list of chars
@@ -13,6 +18,22 @@ namespace io{
      * \return Exit code
      */
     int server_run(int argc, char* argv[]);
+
+    /** 
+     * Setup the wiringPi pins and correct dataflow as well as interrupts
+     * \return Exit code
+     */
+    int setup_io();
+
+    /** 
+     * Interrupt function for the door switch
+     */
+    void door_switch_interrupt(void);
+
+    /**
+     * Door open or closed boolean variable. Edited by door_switch_interrupt.
+     */
+    volatile bool isDoorOpen = false;
 }
 
 #endif
