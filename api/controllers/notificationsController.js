633463fb5d527fb220679f7e03b2b8e94c48939b
iwishiwasaneagle <jh.ewers@gmail.com>
2021-02-24 13:06:34 +0000

    functions for send route

--- api/controllers/notificationsController.js
+++ api/controllers/notificationsController.js
@@ -31,3 +31,31 @@ exports.delete_notification_data = function(req, res) {
     });
   };
      
+exports.send_notification = function(req,res){
+    console.log("send_notification to _id="+req.params.id);
+    Notification.findById(req.params.id, function(err, notification){
+        if(err){
+            res.send(err);
+        }
+        // TODO CHANGE THESE DOWN THE LINE
+        webpush.setVapidDetails(
+            "mailto:jh.ewers@gmail.com",
+            "BN6BhZ2mBQ-oiR78XnNrizLWotzej3iL-TTaTn5egHMmBfqJpdrmbUiIjjy_PsHgacuh3i17Hpgx7LuWwQL9Dvg",
+            "A93a0xUMa4PeoXMZkA8b7iA4vmIkh9-I7AN85DH5G1o"
+        );
+
+        const pushSub = {endpoint:notification.endpoint,
+            keys:notification.keys            
+        };
+        console.log(pushSub);
+
+        webpush.sendNotification(pushSub, JSON.stringify({
+            url: "http://www.google.com",
+            text: "Hi there",
+            image: "https://spyna.it/icons/favicon.ico",
+            tag: "wow",
+            title: "Martin SMelzz"
+         })).then(
+            ()=>res.send("OK")
+        );
+    });
