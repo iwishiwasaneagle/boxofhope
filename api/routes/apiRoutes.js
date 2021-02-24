522fc6989fa673645030cdaa963ee03a7699c5e0
Sarah <sarahswinton99@gmail.com>
2021-02-24 14:14:53 +0000

    Placeholder API Documentation

--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -20,29 +20,117 @@ module.exports = function(app) {
     app.route('/state/UVC')
         .put(state.update_uvc_state);
 
+    /**
+    * This function comment is parsed by doctrine
+    * @route PUT /state/UVC
+    * @group state - Operations about system states
+    * @param {string} endpoint - PushNotification endpoint
+    * @param {string} key.auth - Auth key
+    * @param {string} key.p256dh - p256dh key
+    * @returns {object} 200 - TODO
+    * @returns {Error}  default - Unexpected error
+    */
+
     app.route('/state/UVC/last')
         .get(state.read_uvc_last);
+
+    /**
+    * This function comment is parsed by doctrine
+    * @route PUT /state/UVC
+    * @group state - Operations about system states
+    * @param {string} endpoint - PushNotification endpoint
+    * @param {string} key.auth - Auth key
+    * @param {string} key.p256dh - p256dh key
+    * @returns {object} 200 - TODO
+    * @returns {Error}  default - Unexpected error
+    */
 
     app.route('/state/present-mask')
         .get(state.read_mask_present)
         .put(state.update_mask_present);
 
+    /**
+    * This function comment is parsed by doctrine
+    * @route PUT /state/UVC
+    * @group state - Operations about system states
+    * @param {string} endpoint - PushNotification endpoint
+    * @param {string} key.auth - Auth key
+    * @param {string} key.p256dh - p256dh key
+    * @returns {object} 200 - TODO
+    * @returns {Error}  default - Unexpected error
+    */
+
     app.route('/state/door-switch')
         .get(state.read_switch_open_close)
         .put(state.update_switch_open_close);
 
+    /**
+    * This function comment is parsed by doctrine
+    * @route PUT /state/UVC
+    * @group state - Operations about system states
+    * @param {string} endpoint - PushNotification endpoint
+    * @param {string} key.auth - Auth key
+    * @param {string} key.p256dh - p256dh key
+    * @returns {object} 200 - TODO
+    * @returns {Error}  default - Unexpected error
+    */
+
     app.route('/state/user-home')
         .put(state.update_user_home);
+    
+    /**
+    * This function comment is parsed by doctrine
+    * @route PUT /state/UVC
+    * @group state - Operations about system states
+    * @param {string} endpoint - PushNotification endpoint
+    * @param {string} key.auth - Auth key
+    * @param {string} key.p256dh - p256dh key
+    * @returns {object} 200 - TODO
+    * @returns {Error}  default - Unexpected error
+    */
 
     app.route('/settings/sterilisation-time')
         .get(settings.read_current_sterilisation_time)
         .put(settings.update_sterilisation_time);
 
+    /**
+    * This function comment is parsed by doctrine
+    * @route PUT /state/UVC
+    * @group state - Operations about system states
+    * @param {string} endpoint - PushNotification endpoint
+    * @param {string} key.auth - Auth key
+    * @param {string} key.p256dh - p256dh key
+    * @returns {object} 200 - TODO
+    * @returns {Error}  default - Unexpected error
+    */
+
     app.route('/mask/mask-count')
         .get(mask.read_mask_count);
 
+    /**
+    * This function comment is parsed by doctrine
+    * @route PUT /state/UVC
+    * @group state - Operations about system states
+    * @param {string} endpoint - PushNotification endpoint
+    * @param {string} key.auth - Auth key
+    * @param {string} key.p256dh - p256dh key
+    * @returns {object} 200 - TODO
+    * @returns {Error}  default - Unexpected error
+    */
+
     app.route('/mask/register-new')
         .post(mask.register_new_mask);
+
+    /**
+    * This function comment is parsed by doctrine
+    * @route PUT /state/UVC
+    * @group state - Operations about system states
+    * @param {string} endpoint - PushNotification endpoint
+    * @param {string} key.auth - Auth key
+    * @param {string} key.p256dh - p256dh key
+    * @returns {object} 200 - TODO
+    * @returns {Error}  default - Unexpected error
+    */
 
     app.route('/mask/:maskId')
         .get(mask.read_mask)
@@ -59,8 +147,20 @@ module.exports = function(app) {
     * @returns {object} 200 - TODO
     * @returns {Error}  default - Unexpected error
     */
+
     app.route('/notification/register-new')
         .post(notification.register_new_notification_data);
+
+    /**
+    * This function comment is parsed by doctrine
+    * @route PUT /state/UVC
+    * @group state - Operations about system states
+    * @param {string} endpoint - PushNotification endpoint
+    * @param {string} key.auth - Auth key
+    * @param {string} key.p256dh - p256dh key
+    * @returns {object} 200 - TODO
+    * @returns {Error}  default - Unexpected error
+    */
 
     app.route('/notification/:id')
         .get(notification.read_notification_data)
