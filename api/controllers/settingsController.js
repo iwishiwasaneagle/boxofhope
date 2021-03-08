2874e6d750702a84761470bd2de4bdc458ae0fdf
Sarah <sarahswinton99@gmail.com>
2021-03-08 13:56:20 +0000

    Updated Settings Error Returns

--- api/controllers/settingsController.js
+++ api/controllers/settingsController.js
@@ -7,7 +7,7 @@ Settings = mongoose.model('Settings');
 exports.read_current_sterilisation_time = function(req, res){
     Settings.find({}, function(err,setting){
         if (err)
-            res.send(err);
+            res.status(404).send('Bad Request: Cannot read current sterilisation time.');
         res.json(setting);
     });
     res.status(200);
@@ -16,7 +16,7 @@ exports.read_current_sterilisation_time = function(req, res){
 exports.update_sterilisation_time = function(req, res){
     Settings.findOneAndUpdate({}, req.body, {new:true}, function(err,setting){
         if (err)
-            res.send(err);
+            res.status(404).send('Cannot update sterilisation time');
         res.json(setting);
     });
     res.status(200);
