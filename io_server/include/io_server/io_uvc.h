60e840a7551d3e399a7482a35d831257e3147b5f
iwishiwasaneagle <jh.ewers@gmail.com>
2021-03-09 11:15:10 +0000

    Desctructor. clang-format options

--- io_server/include/io_server/io_uvc.h
+++ io_server/include/io_server/io_uvc.h
@@ -8,66 +8,66 @@
 
 namespace io {
 class UVC_Runnable {
-private:
-  /**
-   * \brief Total time for UVC to stay on
-   */
-  unsigned int time_s;
+  private:
+    /**
+     * \brief Total time for UVC to stay on
+     */
+    unsigned int time_s;
 
-  /**
-   * \brief Control physical LED state
-   *
-   * \param state LED state
-   */
-  void led(bool state);
+    /**
+     * \brief Control physical LED state
+     *
+     * \param state LED state
+     */
+    void led(bool state);
 
-  /**
-   * \brief Handles the logic
-   */
-  void runnable(void);
+    /**
+     * \brief Handles the logic
+     */
+    void runnable(void);
 
-  /**
-   * \brief Runs the thread via ::io::UVC_Runnable::runnable
-   */
-  void thread_runner(void);
+    /**
+     * \brief Runs the thread via ::io::UVC_Runnable::runnable
+     */
+    void thread_runner(void);
 
-  /**
-   * \brief Thread group to hold the thread running the UVC_Runnable::runnable
-   */
-  boost::thread_group thread_group;
+    /**
+     * \brief Thread group to hold the thread running the UVC_Runnable::runnable
+     */
+    boost::thread_group thread_group;
 
-public:
-  /**
-   * \brief Start the threaded UVC_Runnable
-   *
-   */
-  void start(void);
+  public:
+    /**
+     * \brief Start the threaded UVC_Runnable
+     *
+     */
+    void start(void);
 
-  /**
-   * \brief Attach to the thread until it finishes. This BLOCKS the thread that
-   * it is run from.
-   *
-   */
-  void attach(void);
+    /**
+     * \brief Attach to the thread until it finishes. This BLOCKS the thread
+     * that it is run from.
+     *
+     */
+    void attach(void);
 
-  /**
-   * \brief Stop the thread via a interrupt point within the thread.
-   *
-   */
-  void stop(void);
+    /**
+     * \brief Stop the thread via a interrupt point within the thread.
+     *
+     */
+    void stop(void);
 
-  /**
-   * \brief Construct a new UVC_Runnable object.
-   *
-   * \param time_s Total time for UVC to stay on
-   */
-  UVC_Runnable(unsigned int time_s);
+    /**
+     * \brief Construct a new UVC_Runnable object.
+     *
+     * \param time_s Total time for UVC to stay on
+     */
+    UVC_Runnable(unsigned int time_s);
 
-  /**
-   * \brief Destroy the UVC_Runnable object. Ensures all threads are stopped
-   * properly via UVC_Runnable::stop
-   */
-  ~UVC_Runnable(void);
+    /**
+     * \brief Destroy the UVC_Runnable object. Ensures all threads are stopped
+     * properly via UVC_Runnable::stop
+     */
+    ~UVC_Runnable(void);
 };
 } // namespace io
 #endif
