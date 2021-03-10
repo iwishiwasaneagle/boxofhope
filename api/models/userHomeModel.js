610c7b15f57949a03868f4d2b111395c74b2da7c
Sarah <sarahswinton99@gmail.com>
2021-03-10 13:02:34 +0000

    Removed id parameter required - automatically created anyway

--- api/models/userHomeModel.js
+++ api/models/userHomeModel.js
@@ -3,10 +3,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema; 
 
 var userHomeSchema = new Schema ({
-    _id: {
-        type:String,
-        required: true
-    },
     user_status: {
       type: [{
         type: String,
