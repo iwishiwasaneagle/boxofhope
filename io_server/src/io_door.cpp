da70639486b4d0ba9813299a5f2a6ce5d1c546fd
iwishiwasaneagle <jh.ewers@gmail.com>
2021-03-17 10:00:29 +0000

    More work on unifying io_server with API

--- io_server/src/io_door.cpp
+++ io_server/src/io_door.cpp
@@ -12,8 +12,8 @@ unsigned int io::Door_Helper::blocking_wait_for_door(unsigned int time_ms){
 }
 
 bool io::Door_Helper::door_state(void){
-    const int debounceAvgCount = 10; //< The amount of times to sample the door before making a decision on the state.
-    unsigned const int debounceDelay = 20; //< The time between samples. Increase if the output flickers.
+    const int debounceAvgCount = 5; //< The amount of times to sample the door before making a decision on the state.
+    unsigned const int debounceDelay = 10; //< The time between samples. Increase if the output flickers.
         
     // Debounce the switch by checking average input 
     // -> low = 0, high = 1, so we need avg inp > 0.5 to consider door closed
--- io_server/src/utils/api.cpp
+++ io_server/src/utils/api.cpp
@@ -4,54 +4,58 @@
 /*=== Core API ===*/
 /*================*/
 
-size_t API::cUrlWriteCallback(void *contents, size_t size, size_t nmemb, void *userp){ 
+size_t API::cUrlWriteCallback(void *contents, size_t size, size_t nmemb, void *userp){
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
 
-json API::operation(std::string method, std::string url, std::string port, std::string endpoint){
-    return API::operation(method, url, port, endpoint, json::object());
+json API::operation(std::string method, std::string endpoint){
+    return API::operation(method, endpoint, json::object());
 }
-json API::operation(std::string method, std::string url, std::string port, std::string endpoint, json body){
-    return API::operation(method, url, port, endpoint, body, json::object());
+json API::operation(std::string method, std::string endpoint, json body){
+    return API::operation(method, endpoint, body, json::object());
 }
-json API::operation(std::string method, std::string url, std::string port, std::string endpoint, json body, json headers){
+json API::operation(std::string method, std::string endpoint, json body, json headers){
 	CURL *curl;
 	CURLcode res;
 	curl = curl_easy_init();
 	std::string responseBuffer;
 	std::string headerBuffer;
 	if(curl) {
-      //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
-      //curl_easy_setopt(curl, CURLOPT_HEADER, 1);
-	  curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, method.c_str());
-        
-      // TODO Make this a CLI flag 
-      curl_easy_setopt(curl, CURLOPT_PROXY, "127.0.0.1:8080");  
+        //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+        //curl_easy_setopt(curl, CURLOPT_HEADER, 1);
+	    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, method.c_str());
 
-      // TODO Improve this. Currently very easy to break.
-	  std::string path =  url+":"+port+endpoint;
-	  curl_easy_setopt(curl, CURLOPT_URL, path.c_str() );
+        if(!PROXY_URL.empty()){
+            res = curl_easy_setopt(curl, CURLOPT_PROXY, PROXY_URL.c_str());
+            std::cout << "\033[1;45mAPI Util\033[0m\tProxy set to " << PROXY_URL << " | Res = " << res << " | " << curl_easy_strerror(res) << std::endl;
+        }
 
-      // TODO Add headers
-	  struct curl_slist *headers = NULL;
-	  headers = curl_slist_append(headers, "Content-Type: application/json");
-	  curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-	  
-      std::cout << body.dump().c_str() << std::endl; 
-      const std::string payload = body.dump();
-      curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, payload.length());
-      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload.c_str());
+        // TODO Improve this. Currently very easy to break.
+	    std::string path =  API_URL+endpoint;
+	    curl_easy_setopt(curl, CURLOPT_URL, path.c_str() );
 
-      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, API::cUrlWriteCallback);
-      curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseBuffer);
-      curl_easy_setopt(curl, CURLOPT_HEADERDATA, &headerBuffer);
-	  
-	  res = curl_easy_perform(curl);
-        
-      std::string body_delimiter = "\r\n\r\n";
-      responseBuffer = responseBuffer.erase(0,responseBuffer.find(body_delimiter)+body_delimiter.length());
+        // TODO Add headers
+	    struct curl_slist *headers = NULL;
+	    headers = curl_slist_append(headers, "Content-Type: application/json");
+	    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
+        const std::string payload = body.dump();
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, payload.length());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload.c_str());
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, API::cUrlWriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseBuffer);
+        curl_easy_setopt(curl, CURLOPT_HEADERDATA, &headerBuffer);
+
+	    res = curl_easy_perform(curl);
+        std::cout << "\033[1;45mAPI Util\033[0m\tCall to " << path << " | Res = " << res << " | " << curl_easy_strerror(res) << std::endl;
+        if(res!=CURLE_OK){
+            return NULL;
+        }
+
+        // std::string body_delimiter = "\r\n\r\n";
+        // responseBuffer = responseBuffer.erase(0,responseBuffer.find(body_delimiter)+body_delimiter.length()); // This was working but now it doesn't... seems like libcurl has started cutting the reponse down???
 	}
 	curl_easy_cleanup(curl);
     return json::parse(responseBuffer);
@@ -77,12 +81,12 @@ json API::HomeState::update(bool isUserHome){
     }
     json payload;
     payload["user_status"] = state;
-    
-    return API::operation("PUT",API_URL, API_PORT, "/state/user-home", payload);
+
+    return API::operation("POST", "/userHome/user-status", payload);
 }
 
 json API::HomeState::get(void){
-    return API::operation("GET",API_URL, API_PORT, "/state/user-home");
+    return API::operation("GET", "/userHome/user-status");
 }
 
 /*============*/
@@ -105,12 +109,12 @@ json API::MaskState::update(bool isMaskPresent){
     }
     json payload;
     payload["mask_status"] = state;
-    
-    return API::operation("PUT",API_URL, API_PORT, "/state/present-mask", payload);
+
+    return API::operation("PUT", "/state/present-mask", payload);
 }
 
 json API::MaskState::get(void){
-    return API::operation("GET",API_URL, API_PORT, "/state/present-mask");
+    return API::operation("GET", "/state/present-mask");
 }
 
 
@@ -125,10 +129,10 @@ json API::UVCState::set(int sterilizationTime){
 json API::UVCState::update(int sterilizationTime){
     json payload;
     payload["mask_status"] = std::to_string(sterilizationTime);
-    
-    return API::operation("PUT",API_URL, API_PORT, "/state/UVC", payload);
+
+    return API::operation("PUT", "/state/UVC", payload);
 }
 
 json API::UVCState::get(void){
-    return API::operation("GET",API_URL, API_PORT, "/state/UVC/last");
+    return API::operation("GET", "/state/UVC/last");
 }
