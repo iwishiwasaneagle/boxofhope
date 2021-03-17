58b44526aa5511163c4560f5064638b488ef74b5
iwishiwasaneagle <jh.ewers@gmail.com>
2021-03-17 15:19:10 +0000

    Cleaned code up

--- io_server/include/utils/api.h
+++ io_server/include/utils/api.h
@@ -1,12 +1,12 @@
 #ifndef UTILS_API_H
 #define UTILS_API_H
 
-#include <string>
-#include <ostream>
-#include <istream>
-#include <iostream>
-#include <stdexcept>
 #include <curl/curl.h>
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <stdexcept>
+#include <string>
 
 #include <nlohmann/json.hpp>
 
@@ -18,153 +18,173 @@ using json = nlohmann::json;
 inline std::string API_URL = "";
 inline std::string PROXY_URL = "";
 
-class API{
-    private:
-        /**
-         * \brief Callback for cURL [CURLOPT_WRITEFUNCTION](https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html)
-         *
-         * \param in contents Points to the delivered data
-         * \param in size Always 1
-         * \param in nmemb Size of the data
-         * \param out userp A data pointer to pass to the write callback. A pointer for this is passed to [CURLOPT_WRITEDATA](https://curl.se/libcurl/c/CURLOPT_WRITEDATA.html)
-         *
-         * \return The number of bytes actually taken care of. If that amount differs from the amount passed to your callback function, it'll signal an error condition to libcurl.
-         */
-		static size_t cUrlWriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
+class API {
+  private:
+    /**
+     * \brief Callback for cURL
+     * [CURLOPT_WRITEFUNCTION](https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html)
+     *
+     * \param in contents Points to the delivered data
+     * \param in size Always 1
+     * \param in nmemb Size of the data
+     * \param out userp A data pointer to pass to the write callback. A pointer
+     * for this is passed to
+     * [CURLOPT_WRITEDATA](https://curl.se/libcurl/c/CURLOPT_WRITEDATA.html)
+     *
+     * \return The number of bytes actually taken care of. If that amount
+     * differs from the amount passed to your callback function, it'll signal an
+     * error condition to libcurl.
+     */
+    static size_t cUrlWriteCallback(void *contents, size_t size, size_t nmemb,
+                                    void *userp);
 
+    /**
+     * \brief Generic static function for ::API members to use for running HTTP
+     * requests.
+     *
+     * \param method The HTTP method to use (GET, POST, etc)
+     * \param endpoint API endpoint beginning with a '/'
+     * \param body Request body as a ::json object
+     * \param headers Request headers as a ::json object
+     *
+     * \return ::json object holding the request message
+     */
+    static json operation(std::string method, std::string endpoint, json body,
+                          json headers);
 
-        /**
-         * \brief Generic static function for ::API members to use for running HTTP requests.
-         *
-         * \param method The HTTP method to use (GET, POST, etc)
-         * \param endpoint API endpoint beginning with a '/'
-         * \param body Request body as a ::json object
-         * \param headers Request headers as a ::json object
-         *
-         * \return ::json object holding the request message 
-         */
-        static json operation(std::string method, std::string endpoint, json body, json headers);
+    /**
+     * \brief Generic static function for ::API members to use for running HTTP
+     * requests.
+     *
+     * Passes a empty header ::json object to ::API::operation(std::string,
+     * std::string, json, json).
+     *
+     * \param method The HTTP method to use (GET, POST, etc)
+     * \param endpoint API endpoint beginning with a '/'
+     * \param body Request body as a ::json object
+     *
+     * \return ::json object holding the request message
+     */
+    static json operation(std::string method, std::string endpoint, json body);
 
-        /**
-         * \brief Generic static function for ::API members to use for running HTTP requests.
-         *
-         * Passes a empty header ::json object to ::API::operation(std::string, std::string, json, json).
-         *
-         * \param method The HTTP method to use (GET, POST, etc)
-         * \param endpoint API endpoint beginning with a '/'
-         * \param body Request body as a ::json object
-         *
-         * \return ::json object holding the request message 
-         */
-        static json operation(std::string method, std::string endpoint, json body);
-        
-        /**
-         * \brief Generic static function for ::API members to use for running HTTP requests.
-         *
-         * Passes a empty body ::json object to ::API::operation(std::string, std::string, json).
-         *
-         * \param method The HTTP method to use (GET, POST, etc)
-         * \param endpoint API endpoint beginning with a '/'
-         *
-         * \return ::json object holding the request message 
-         */
-        static json operation(std::string method, std::string endpoint);
-        
-        /**
-         * \brief Template struct to allow a consistent API interface to be made. All states are single value, and therefore T can be bool, int, etc.
-         *
-         */
-        template<class T>
-        struct State{
-                virtual json update(T state){throw std::runtime_error("static json update(bool state) is not implemeted yet.");};
-                virtual json set(T state){throw std::runtime_error("static json set(bool state) is not implemeted yet.");};
-                virtual json get(void){throw std::runtime_error("static json get(bool state) is not implemeted yet.");};
-        
+    /**
+     * \brief Generic static function for ::API members to use for running HTTP
+     * requests.
+     *
+     * Passes a empty body ::json object to ::API::operation(std::string,
+     * std::string, json).
+     *
+     * \param method The HTTP method to use (GET, POST, etc)
+     * \param endpoint API endpoint beginning with a '/'
+     *
+     * \return ::json object holding the request message
+     */
+    static json operation(std::string method, std::string endpoint);
+
+    /**
+     * \brief Template struct to allow a consistent API interface to be made.
+     * All states are single value, and therefore T can be bool, int, etc.
+     *
+     */
+    template <class T> struct State {
+        virtual json update(T state) {
+            throw std::runtime_error(
+                "static json update(bool state) is not implemeted yet.");
         };
-    public:
-        /**
-         * \brief Class to access the /state/user-home endpoint
-         */
-        class HomeState: public State<bool> {
-			public:
-                /**
-                 * \brief Update the state
-                 *
-                 * \param isUserHome Is user home?
-                 *
-                 * \return ::json object with message data
-                 */
-	            json update(bool isUserHome);        
-                /**
-                 * \brief Set the initial state
-                 *
-                 * \param isUserHome Is user home?
-                 *
-                 * \return ::json object with message data
-                 */
-                json set(bool isUserHome);
-                /**
-                 * \brief Get the state
-                 *
-                 * \return ::json object with message data
-                 */
-                json get(void);
+        virtual json set(T state) {
+            throw std::runtime_error(
+                "static json set(bool state) is not implemeted yet.");
         };
-        /**
-         * \brief Class to access the /state/present-mask endpoint
-         */
-        class MaskState: public State<bool> {
-			public:
-                /**
-                 * \brief Update the state
-                 *
-                 * \param isMaskPresent Is mask present?
-                 *
-                 * \return ::json object with message data
-                 */
-	            json update(bool isMaskPresent);        
-                /**
-                 * \brief Set the initial state
-                 *
-                 * \param isMaskPresent Is mask present?
-                 *
-                 * \return ::json object with message data
-                 */
-                json set(bool isMaskPresent);
-                /**
-                 * \brief Get the state
-                 *
-                 * \return ::json object with message data
-                 */
-                json get(void);
+        virtual json get(void) {
+            throw std::runtime_error(
+                "static json get(bool state) is not implemeted yet.");
         };
+    };
+
+  public:
+    /**
+     * \brief Class to access the /state/user-home endpoint
+     */
+    class HomeState : public State<bool> {
+      public:
         /**
-         * \brief Class to access the /state/uvc endpoint
+         * \brief Update the state
+         *
+         * \param isUserHome Is user home?
+         *
+         * \return ::json object with message data
          */
-        class UVCState: public State<int> {
-			public:
-                /**
-                 * \brief Update the state
-                 *
-                 * \param sterilizationTime End time of sterilization.
-                 *
-                 * \return ::json object with message data
-                 */
-	            json update(int sterilizationTime);        
-                /**
-                 * \brief Set the initial state
-                 *
-                 * \param sterilizationtime End time of sterilization.
-                 *
-                 * \return ::json object with message data
-                 */
-                json set(int sterilizationTime);
-                /**
-                 * \brief Get the last sterilization time.
-                 *
-                 * \return ::json object with message data
-                 */
-                json get(void);
-        };
+        json update(bool isUserHome);
+        /**
+         * \brief Set the initial state
+         *
+         * \param isUserHome Is user home?
+         *
+         * \return ::json object with message data
+         */
+        json set(bool isUserHome);
+        /**
+         * \brief Get the state
+         *
+         * \return ::json object with message data
+         */
+        json get(void);
+    };
+    /**
+     * \brief Class to access the /state/present-mask endpoint
+     */
+    class MaskState : public State<bool> {
+      public:
+        /**
+         * \brief Update the state
+         *
+         * \param isMaskPresent Is mask present?
+         *
+         * \return ::json object with message data
+         */
+        json update(bool isMaskPresent);
+        /**
+         * \brief Set the initial state
+         *
+         * \param isMaskPresent Is mask present?
+         *
+         * \return ::json object with message data
+         */
+        json set(bool isMaskPresent);
+        /**
+         * \brief Get the state
+         *
+         * \return ::json object with message data
+         */
+        json get(void);
+    };
+    /**
+     * \brief Class to access the /state/uvc endpoint
+     */
+    class UVCState : public State<int> {
+      public:
+        /**
+         * \brief Update the state
+         *
+         * \param sterilizationTime End time of sterilization.
+         *
+         * \return ::json object with message data
+         */
+        json update(int sterilizationTime);
+        /**
+         * \brief Set the initial state
+         *
+         * \param sterilizationtime End time of sterilization.
+         *
+         * \return ::json object with message data
+         */
+        json set(int sterilizationTime);
+        /**
+         * \brief Get the last sterilization time.
+         *
+         * \return ::json object with message data
+         */
+        json get(void);
+    };
 };
 #endif
--- io_server/src/utils/api.cpp
+++ io_server/src/utils/api.cpp
@@ -4,41 +4,45 @@
 /*=== Core API ===*/
 /*================*/
 
-size_t API::cUrlWriteCallback(void *contents, size_t size, size_t nmemb, void *userp){
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
+size_t API::cUrlWriteCallback(void *contents, size_t size, size_t nmemb,
+                              void *userp) {
+    ((std::string *)userp)->append((char *)contents, size * nmemb);
     return size * nmemb;
 }
 
-json API::operation(std::string method, std::string endpoint){
+json API::operation(std::string method, std::string endpoint) {
     return API::operation(method, endpoint, json::object());
 }
-json API::operation(std::string method, std::string endpoint, json body){
+json API::operation(std::string method, std::string endpoint, json body) {
     return API::operation(method, endpoint, body, json::object());
 }
-json API::operation(std::string method, std::string endpoint, json body, json headers){
-	CURL *curl;
-	CURLcode res;
-	curl = curl_easy_init();
-	std::string responseBuffer;
-	std::string headerBuffer;
-	if(curl) {
-        //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
-        //curl_easy_setopt(curl, CURLOPT_HEADER, 1);
-	    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, method.c_str());
+json API::operation(std::string method, std::string endpoint, json body,
+                    json headers) {
+    CURL *curl;
+    CURLcode res;
+    curl = curl_easy_init();
+    std::string responseBuffer;
+    std::string headerBuffer;
+    if (curl) {
+        // curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+        // curl_easy_setopt(curl, CURLOPT_HEADER, 1);
+        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, method.c_str());
 
-        if(!PROXY_URL.empty()){
+        if (!PROXY_URL.empty()) {
             res = curl_easy_setopt(curl, CURLOPT_PROXY, PROXY_URL.c_str());
-            std::cout << "\033[1;45mAPI Util\033[0m\tProxy set to " << PROXY_URL << " | Res = " << res << " | " << curl_easy_strerror(res) << std::endl;
+            std::cout << "\033[1;45mAPI Util\033[0m\tProxy set to " << PROXY_URL
+                      << " | Res = " << res << " | " << curl_easy_strerror(res)
+                      << std::endl;
         }
 
         // TODO Improve this. Currently very easy to break.
-	    std::string path =  API_URL+endpoint;
-	    curl_easy_setopt(curl, CURLOPT_URL, path.c_str() );
+        std::string path = API_URL + endpoint;
+        curl_easy_setopt(curl, CURLOPT_URL, path.c_str());
 
         // TODO Add headers
-	    struct curl_slist *headers = NULL;
-	    headers = curl_slist_append(headers, "Content-Type: application/json");
-	    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        struct curl_slist *headers = NULL;
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
         const std::string payload = body.dump();
         curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, payload.length());
@@ -48,16 +52,21 @@ json API::operation(std::string method, std::string endpoint, json body, json he
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseBuffer);
         curl_easy_setopt(curl, CURLOPT_HEADERDATA, &headerBuffer);
 
-	    res = curl_easy_perform(curl);
-        std::cout << "\033[1;45mAPI Util\033[0m\tCall to " << path << " | Res = " << res << " | " << curl_easy_strerror(res) << std::endl;
-        if(res!=CURLE_OK){
+        res = curl_easy_perform(curl);
+        std::cout << "\033[1;45mAPI Util\033[0m\tCall to " << path
+                  << " | Res = " << res << " | " << curl_easy_strerror(res)
+                  << std::endl;
+        if (res != CURLE_OK) {
             return NULL;
         }
 
         // std::string body_delimiter = "\r\n\r\n";
-        // responseBuffer = responseBuffer.erase(0,responseBuffer.find(body_delimiter)+body_delimiter.length()); // This was working but now it doesn't... seems like libcurl has started cutting the reponse down???
-	}
-	curl_easy_cleanup(curl);
+        // responseBuffer =
+        // responseBuffer.erase(0,responseBuffer.find(body_delimiter)+body_delimiter.length());
+        // // This was working but now it doesn't... seems like libcurl has
+        // started cutting the reponse down???
+    }
+    curl_easy_cleanup(curl);
     return json::parse(responseBuffer);
 }
 
@@ -65,19 +74,19 @@ json API::operation(std::string method, std::string endpoint, json body, json he
 /*=== Home ===*/
 /*============*/
 
-json API::HomeState::set(bool isUserHome){
+json API::HomeState::set(bool isUserHome) {
     return API::HomeState::update(isUserHome);
 }
 
-json API::HomeState::update(bool isUserHome){
+json API::HomeState::update(bool isUserHome) {
     std::string state;
-    switch(isUserHome){
-        case true:
-            state="User Home";
-            break;
-        case false:
-            state="User Not Home";
-            break;
+    switch (isUserHome) {
+    case true:
+        state = "User Home";
+        break;
+    case false:
+        state = "User Not Home";
+        break;
     }
     json payload;
     payload["user_status"] = state;
@@ -85,7 +94,7 @@ json API::HomeState::update(bool isUserHome){
     return API::operation("POST", "/userHome/user-status", payload);
 }
 
-json API::HomeState::get(void){
+json API::HomeState::get(void) {
     return API::operation("GET", "/userHome/user-status");
 }
 
@@ -93,19 +102,19 @@ json API::HomeState::get(void){
 /*=== Mask ===*/
 /*============*/
 
-json API::MaskState::set(bool isMaskPresent){
+json API::MaskState::set(bool isMaskPresent) {
     return API::MaskState::update(isMaskPresent);
 }
 
-json API::MaskState::update(bool isMaskPresent){
+json API::MaskState::update(bool isMaskPresent) {
     std::string state;
-    switch(isMaskPresent){
-        case true:
-            state="Mask Present";
-            break;
-        case false:
-            state="No Mask Present";
-            break;
+    switch (isMaskPresent) {
+    case true:
+        state = "Mask Present";
+        break;
+    case false:
+        state = "No Mask Present";
+        break;
     }
     json payload;
     payload["mask_status"] = state;
@@ -113,26 +122,25 @@ json API::MaskState::update(bool isMaskPresent){
     return API::operation("PUT", "/state/present-mask", payload);
 }
 
-json API::MaskState::get(void){
+json API::MaskState::get(void) {
     return API::operation("GET", "/state/present-mask");
 }
-
 
 /*===========*/
 /*=== UVC ===*/
 /*===========*/
 
-json API::UVCState::set(int sterilizationTime){
+json API::UVCState::set(int sterilizationTime) {
     return API::UVCState::update(sterilizationTime);
 }
 
-json API::UVCState::update(int sterilizationTime){
+json API::UVCState::update(int sterilizationTime) {
     json payload;
     payload["mask_status"] = std::to_string(sterilizationTime);
 
     return API::operation("PUT", "/state/UVC", payload);
 }
 
-json API::UVCState::get(void){
+json API::UVCState::get(void) {
     return API::operation("GET", "/state/UVC/last");
 }
