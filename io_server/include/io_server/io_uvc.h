9d334b8481db2fe5762d07bdc7435bc93d7b1c1d
iwishiwasaneagle <jh.ewers@gmail.com>
2021-03-29 15:41:57 +0100

    Docs

--- io_server/include/io_server/io_uvc.h
+++ io_server/include/io_server/io_uvc.h
@@ -2,6 +2,11 @@
 #define IO_UVC_H
 
 namespace io {
+
+/**
+ * \brief UVC runnable class to encapsulate all things Uvc -> updating server, pulling data, checking if needed to run
+ * \test None
+ */
 class UVC_Runnable : public GenericRunnable {
   private:
     /**
