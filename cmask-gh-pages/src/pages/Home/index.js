286bc5f627bed3c16a8d1cd55eb31f5f3d1839f8
martinkarel <martin.karel13@gmail.com>
2021-04-04 01:16:55 +0100

    New content structure

--- cmask-gh-pages/src/pages/Home/index.js
+++ cmask-gh-pages/src/pages/Home/index.js
@@ -2,6 +2,7 @@ import { lazy } from "react";
 
 import IntroContent from "../../content/IntroContent.json";
 import MiddleBlockContent from "../../content/MiddleBlockContent.json";
+import TechContent from "../../content/TechContent.json";
 import AboutContent from "../../content/AboutContent.json";
 import MissionContent from "../../content/MissionContent.json";
 import ProductContent from "../../content/ProductContent.json";
@@ -30,6 +31,13 @@ const Home = () => {
         button={MiddleBlockContent.button}
       />
       <ContentBlock
+        type="right"
+        title={MissionContent.title}
+        content={MissionContent.text}
+        icon="product-launch.svg"
+        id="mission"
+      />
+      <ContentBlock
         type="left"
         title={AboutContent.title}
         content={AboutContent.text}
@@ -37,14 +45,6 @@ const Home = () => {
         icon="graphs.svg"
         id="about"
       />
-      <ContentBlock
-        type="right"
-        title={MissionContent.title}
-        content={MissionContent.text}
-        icon="product-launch.svg"
-        id="mission"
-      />
-
       <ContentBlock
         type="left"
         title={ProductContent.title}
