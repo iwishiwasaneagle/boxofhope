dcbeb0e27ed536537cd41ad47937f2967c2729ea
Sarah <sarahswinton99@gmail.com>
2021-04-13 19:37:32 +0100

    Added settings runnable to return max-wear count

--- api/runners/settingsRunnable.js
+++ api/runners/settingsRunnable.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose'),
     Settings = mongoose.model('Settings'); 
 
     exports.get_max_washes = () =>{
-
+        return Settings.find({"keyword": "max-wears"}, "value").sort({"createdAt":-1}).limit(1).then(function(setting){
+            return(setting[0].value);
+        });
     }
     
